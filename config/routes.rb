c696c0de88009eedbe8917f9b4f598b71ddc5a58
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2016-08-16 16:21:31 +0100

    added login logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+    # These routes will be for signup. The first renders a form in the browse, the second will
+    # receive the form and create a user in our database using the data given to us by the user.
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
 end
+
+
